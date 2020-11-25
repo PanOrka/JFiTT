ed85c79c8a0fca5a6266ebe3a6650a9d84516546
Eryk <37209877+PanOrka@users.noreply.github.com>
2020-11-26 00:34:00 +0100

    Add files via upload

--- lab3/mod_oper.c
+++ lab3/mod_oper.c
@@ -3,21 +3,20 @@
 #include "mod_oper.h"
 
 /**
- * 
- * 
- * USE EXTENDED EUCLID's ALGORITHM HERE
- * Instead of this naive shit
- * 
+ * EXTENDED EUCLID's ALGORITHM
 */
 int inv_mulmod(int a) {
     a = a >= 0 ? a : MOD_G + a;
-    for (int i=1; i<MOD_G; ++i) {
-        if (mul_mod(a, i) == 1) {
-            return i;
-        }
+
+    int x = 0, y = 1, u = 1, v = 0;
+    int b = MOD_G;
+    while (a != 0) {
+        int q = b / a, r = b % a;
+        int m = x - u * q, n = y - v * q;
+        b = a, a = r, x = u, y = v, u = m, v = n;
     }
 
-    return 0;
+    return x / b;
 }
 
 int add_mod(int a, int b) {
@@ -29,12 +28,20 @@ int sub_mod(int a, int b) {
 }
 
 int mul_mod(int a, int b) {
-    return (a * b) % MOD_G;
+    int acc = 0;
+    b = b >= 0 ? b : MOD_G + b;
+
+    for (int i=0; i<b; ++i) {
+        acc = add_mod(acc, a);
+    }
+
+    return acc;
 }
 
 int div_mod(int a, int b) {
-    return a * inv_mulmod(b);
+    return mul_mod(a, inv_mulmod(b));
 }
+
 int pow_mod(int a, int b) {
     bool inv;
     b = (inv = b < 0) ? -b : b;
